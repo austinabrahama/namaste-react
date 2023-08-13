d15f7e8885b0d709f591ef96af6e378d42788d8c
Austin Abraham <austin-9903@csez.zohocorpin.com>
2023-08-13 19:26:52 +0530

    header added

--- App.js
+++ App.js
@@ -2,19 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// const Heading = () => {
-//     return <h1>Hi Austin</h1>;
-// };
-
-const Heading = () => <h1>Hi Austin</h1>;
-
-const Content = () => {
+const Header = () => {
     return (
-        <div>
-            <Heading />
-            <h1>What are you doing?</h1>
+        <div className="header">
+            <div className="logo-container">
+                <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=" className="logo"></img>
+            </div>
+            <div className="nav-container">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
     );
 };
 
-root.render(<Content />);
+const AppLayout = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+root.render(<AppLayout />);
